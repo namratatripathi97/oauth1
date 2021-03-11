f5b40bb5ec3c0e1e18db788da978ff8ccca79a2a
namratatripathi97 <80440869+namratatripathi97@users.noreply.github.com>
2021-03-11 17:02:18 +0530

    Update image-upload.blade.php
    titlte change

--- resources/views/image-upload.blade.php
+++ resources/views/image-upload.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Laravel 7 S3 Image Upload tutorial example - XpertPhp</title>
+  <title>image-uploader</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
@@ -9,7 +9,7 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
- <div class="container">
+ <div class="container p-10">
  <div class="row">
  <div class="col-lg-12">
  @if ($message = Session::get('success'))
